020e1a7a53a19ad552436466df96bf13e4d38629
donvico <wvictoryc@gmail.com>
2023-08-25 13:41:12 +0100

    commit

---
+++ realtime.js
@@ -0,0 +1,70 @@
+
+function retrieveMessage(content) {
+    const messages = localStorage.getItem(content);
+    
+    if (messages) {
+        return JSON.parse(messages);
+    } else {
+        return [];
+    }
+}
+
+
+function storeContent(content, messages) {
+    localStorage.setItem(content, JSON.stringify(messages));
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const messageContent1 = document.getElementById('messageContent');
+    const messageContent2 = document.getElementById('messageContent2');
+    
+    const textUser1 = document.getElementById('textUser1');
+    const textUser2 = document.getElementById('textUser2');
+    
+    const userBtn1 = document.querySelector('#user1 button');
+    const userBtn2 = document.querySelector('#user2 button');
+    
+    userBtn1.addEventListener('click', function () {
+        const message = textUser1.value;
+        if (message) {
+            const messages = retrieveMessage('user1Messages');
+            messages.push({ sender: 'User 1', message });
+            storeContent('user1Messages', messages);
+            
+            appendMessage(messageContent1, 'User 1', message);
+            appendMessage(messageContent2, 'User 1', message);
+            
+            textUser1.value = '';
+        }
+    });
+    
+    userBtn2.addEventListener('click', function () {
+        const message = textUser2.value;
+        if (message) {
+            const messages = retrieveMessage('user2Messages');
+            messages.push({ sender: 'User 2', message });
+            storeContent('user2Messages', messages);
+            
+            appendMessage(messageContent2, 'User 2', message);
+            appendMessage(messageContent1, 'User 2', message);
+            
+            textUser2.value = '';
+        }
+    });
+    
+    retrieveMessage('user1Messages').forEach(({ sender, message }) => {
+        appendMessage(messageContent1, sender, message);
+        appendMessage(messageContent2, sender, message);
+    });
+    
+    retrieveMessage('user2Messages').forEach(({ sender, message }) => {
+        appendMessage(messageContent2, sender, message);
+        appendMessage(messageContent1, sender, message);
+    });
+});
+
+function appendMessage(container, sender, message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+    container.appendChild(messageElement);
+}
